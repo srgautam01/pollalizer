ee00a8bfb64f8339f9761d7429fc3a520d0b9a33
sundar <sundar.gautam@selu.edu>
2021-03-22 09:39:59 -0500

    choices available to add in Django admin.

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
