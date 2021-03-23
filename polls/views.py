3c8a78811dd37138780db61e33d582d67a2eb832
sundar <sundar.gautam@selu.edu>
2021-03-23 13:19:37 -0500

    Bootstrap used and readability of the pages implemented where users can choose the choices and vote them and get the vote results of the choices.

--- polls/views.py
+++ polls/views.py
@@ -13,7 +13,7 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions(not including those set to be published in the future)."""
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:20]
 
 
 class DetailView(generic.DetailView):
@@ -33,8 +33,21 @@ class ResultView(generic.DetailView):
 
 
 def vote(request, question_id):
-    model = Question
-    template_name = 'polls/results.html'
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError,Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {'question':question,
+                                                      'error_message':"You didn't select a choice.",
+                                                })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+    # Always return an HttpResponseRedirect after successfully dealing
+    # with POST data. This prevents data from being posted twice if a
+    # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
 
 
 # def index(request):
