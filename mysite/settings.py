14ddd812ee4328e988a111c0a29b127762064aa0
sundar <sundar.gautam@selu.edu>
2021-03-31 13:17:52 -0500

    changed default db to sqlite

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,19 +75,19 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # },
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'polls',
-        'PASSWORD': 'pollpolls',
-        'HOST': 'db',
-        'PORT': '5432',
-
-    }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'postgres',
+    #     'USER': 'polls',
+    #     'PASSWORD': 'pollpolls',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    #
+    # }
 }
 
 
